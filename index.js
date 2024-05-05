621340ff801449d5da199fc59b80d5af1c841caf
gyheudjik <gs1kabotaig@hotmail.com>
2024-05-06 05:59:12 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-function canFinish(numCourses, prerequisites) {
-  const graph = new Map();
-  const visited = new Array(numCourses).fill(0);
-  for (const [course, prerequisite] of prerequisites) {
-    if (!graph.has(course)) graph.set(course, []);
-    graph.get(course).push(prerequisite);
-  }
-  for (let i = 0; i < numCourses; i++) {
-    if (!dfs(i)) return false;
-  }
-  return true;
-  function dfs(course) {
-    if (visited[course] === 1) return false;
-    if (visited[course] === -1) return true;
-    visited[course] = 1;
-    if (graph.has(course)) {
-      for (const prerequisite of graph.get(course)) {
-        if (!dfs(prerequisite)) return false;
-      }
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = Array.from(Array(m), () => Array(n).fill(0));
+  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) continue;
+      if (i > 0) dp[i][j] += dp[i - 1][j];
+      if (j > 0) dp[i][j] += dp[i][j - 1];
     }
-    visited[course] = -1;
-    return true;
   }
+  return dp[m - 1][n - 1];
 }
